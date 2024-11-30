3f71b10a60e1611fc8403db04e2bd329d5b2b092
ZayraGS1403 <142755654+ZayraGS1403@users.noreply.github.com>
2024-11-30 10:30:10 -0500

    feat:add delete test and add documentation prestamo controller

--- src/test/java/com/bichotas/moduloprestamos/service/PrestamoServiceTest.java
+++ src/test/java/com/bichotas/moduloprestamos/service/PrestamoServiceTest.java
@@ -97,4 +97,56 @@ class PrestamoServiceTest {
         assertEquals("Prestado", prestamosWithStatusPrestado.get(0).getEstado());
         assertEquals("Prestado", prestamosWithStatusPrestado.get(1).getEstado());
     }
+
+    @Test
+    public void shouldDeletePrestamoSuccess() {
+        Prestamo prestamo = new Prestamo();
+        prestamo.setIdEstudiante("123");
+        prestamo.setIdLibro("456");
+        prestamo.setEstado("Prestado");
+
+        when(prestamoRepository.findById(any())).thenReturn(java.util.Optional.of(prestamo));
+
+        Prestamo result = prestamoService.deletePrestamoById("123");
+
+        assertNotNull(result);
+        verify(prestamoRepository, times(1)).deleteById(prestamo.getId());
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenPrestamoIsReturned() {
+        Prestamo prestamo = new Prestamo();
+        prestamo.setIdEstudiante("123");
+        prestamo.setIdLibro("456");
+        prestamo.setEstado("Devuelto");
+
+        when(prestamoRepository.findById(any())).thenReturn(java.util.Optional.of(prestamo));
+
+        assertThrows(PrestamosException.PrestamosExceptionStateError.class, () -> {
+            prestamoService.deletePrestamoById("123");
+        });
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenPrestamoIsOverdue() {
+        Prestamo prestamo = new Prestamo();
+        prestamo.setIdEstudiante("123");
+        prestamo.setIdLibro("456");
+        prestamo.setEstado("Vencido");
+
+        when(prestamoRepository.findById(any())).thenReturn(java.util.Optional.of(prestamo));
+
+        assertThrows(PrestamosException.PrestamosExceptionStateError.class, () -> {
+            prestamoService.deletePrestamoById("123");
+        });
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenPrestamoNotFound() {
+        when(prestamoRepository.findById(any())).thenReturn(java.util.Optional.empty());
+
+        assertThrows(PrestamosException.PrestamosExceptionPrestamoIdNotFound.class, () -> {
+            prestamoService.deletePrestamoById("123");
+        });
+    }
 }
